7081a5d69e42cf0efc8e5eb15d4cf478479a3c63
Mark <596937450@qq.com>
2022-04-17 14:38:23 +0800

    Start 项目

---
+++ demo_reportlab.py
@@ -0,0 +1,12 @@
+from reportlab import pdfgen
+
+
+class DemoReportLab(object):
+    """docstring for DemoReportLab"""
+
+    def __init__(self):
+        super(DemoReportLab, self).__init__()
+
+
+if __name__ == "__main__":
+    pass
